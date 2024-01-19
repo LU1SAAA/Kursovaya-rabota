859b96a874bb9cc7e6337f54211a25189add7190
PK303-7 <PK303-7@gmail.ru>
2024-01-19 12:02:26 +0300

    hi

---
+++ TEST ALK/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="style/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cat</title>
+    <link rel="icon" href="https://w.forfun.com/fetch/99/992ac857fb51fea944e3f4d388ef5477.jpeg">
+</head>
+<body>
+    <header>
+        <img width="150px" height="100px" src="https://w.forfun.com/fetch/99/992ac857fb51fea944e3f4d388ef5477.jpeg" alt="логотип">
+        <h1>CATTT </h1>
+    </header>
+
+</body>
+</html>
